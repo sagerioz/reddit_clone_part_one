203e17d3b7322a3365cebf5a73e1a5cfa57a3db9
sagerioz <ericaepperson27@yahoo.com>
2017-05-06 14:33:45 -0600

    sort by options good

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ vm.$onInit = function() {
     author: "anonymous",
     img: "https://s15-us2.ixquick.com/cgi-bin/serveimage?url=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2Fb3%2F22%2Fb7%2Fb322b73e57213ea9ea974b5f4f2fc0d7.jpg&sp=de1cf06982e249d02f76dc21e9fef236",
     body: "This is super funny.",
-    date: moment().subtract(2, 'days').subtract(3, 'hours').calendar(),
-    votes: 10,
+    date: moment().subtract(6, 'days').subtract(3, 'hours').calendar(),
+    votes: 1,
     comments: [
       {
         author: "Matt",
@@ -33,7 +33,7 @@ vm.$onInit = function() {
     newCommentVisible: false
     },
     {
-      title: "Another Funny Joke",
+      title: "Yet Another Funny Joke",
       author: "From me to you",
       img: "https://s16-us2.ixquick.com/cgi-bin/serveimage?url=http%3A%2F%2Ft2.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQjlN8RKRWBsusUSiKUSCFX5Nk2ZMwWoI1Ad543XLltpxmOILvP&sp=8b2e1a83cbcf6e050345e8b3327037a5&anticache=950096",
       body: "This is super funny, yah?",
@@ -53,7 +53,7 @@ vm.$onInit = function() {
       author: "From me to you",
       img: "https://s16-us2.ixquick.com/cgi-bin/serveimage?url=http%3A%2F%2Ft1.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQBexuC_MOiFLwAxNmt45O5REwXoaegdIrYaVbAFOLOeu9w4lXw&sp=5a46518ca78bb8de1c27422a79c85254&anticache=626272",
       body: "Not so funny.",
-      date: moment().subtract(2, 'days').subtract(3, 'hours').calendar(),
+      date: moment().subtract(9, 'days').subtract(3, 'hours').calendar(),
       votes: 10,
       comments: [
         {
@@ -70,7 +70,7 @@ vm.$onInit = function() {
       img: "https://s15-us2.ixquick.com/cgi-bin/serveimage?url=http%3A%2F%2Ft0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcQljfaflqEeB_IrmbD4Vgd7rRS_1pIEyaRRmMcT5iXea1zBTn3CSw&sp=66a363996a942b7817b0db54898687e1&anticache=585092",
       body: "Tell me a joke!.",
       date: moment().subtract(2, 'days').subtract(3, 'hours').calendar(),
-      votes: 10,
+      votes: 111,
       comments: [],
       commentsVisible: false,
       newCommentVisible: false
@@ -90,7 +90,16 @@ vm.toggleForm = function () {
 
 
 
-vm.countVotes = function (){
+vm.countVotesUp = function (posts){
+  let temp = posts.votes
+   posts.votes += 1
+   console.log("UP",temp);
+
+}
+vm.countVotesDown = function (posts){
+  let temp = posts.votes
+   posts.votes -= 1
+   console.log("DOWN",temp);
 
 }
 
